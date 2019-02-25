87470654134f007c3540bb710ca861efc5abb3a7
Desmond <user.example.com>
2019-02-25 16:38:36 +0000

    use view holder pattern for optimization of the the listview

--- app/src/main/java/com/training/listviewadapter/PersonAdapter.java
+++ app/src/main/java/com/training/listviewadapter/PersonAdapter.java
@@ -21,27 +21,46 @@ public class PersonAdapter extends ArrayAdapter<Person> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
+        // Get the data item for this position
         Person person = personList.get(position);
 
+        // Check if an existing view is being reused, otherwise inflate the view
         View row = convertView;
-
+        PersonAdapterViewHolder viewHolder;
         if (row == null) {
+            // If there's no view to re-use, inflate a brand new view for row
             row = LayoutInflater.from(getContext()).inflate(R.layout.list_item_layout, parent, false);
+            viewHolder  = new PersonAdapterViewHolder(row);
+            row.setTag(viewHolder);
+        } else {
+            viewHolder = (PersonAdapterViewHolder) row.getTag();
         }
 
-        setRow(person, row);
+        setRow(person, viewHolder);
 
+        // Return the completed view to render on screen
         return row;
     }
 
-    private void setRow(Person person, View row) {
-        TextView nameTextView = row.findViewById(R.id.name_text_view);
-        nameTextView.setText(person.getName());
+    private void setRow(Person person, PersonAdapterViewHolder viewHolder) {
+        // Populate the data from the data object via the viewHolder object
+        // into the template view.
+        viewHolder.nameTextView.setText(person.getName());
+        viewHolder.ageTextView.setText(String.valueOf(person.getAge()));
+        viewHolder.genderTextView.setText(person.getGender());
+    }
 
-        TextView ageTextView = row.findViewById(R.id.age_text_view);
-        ageTextView.setText(String.valueOf(person.getAge()));
 
-        TextView genderTextView = row.findViewById(R.id.gender_text_view);
-        genderTextView.setText(person.getGender());
+    public static class PersonAdapterViewHolder {
+
+        TextView nameTextView;
+        TextView ageTextView;
+        TextView genderTextView;
+
+        public PersonAdapterViewHolder(View currentRow) {
+            nameTextView = currentRow.findViewById(R.id.name_text_view);
+            ageTextView = currentRow.findViewById(R.id.age_text_view);
+            genderTextView = currentRow.findViewById(R.id.gender_text_view);
+        }
     }
 }
